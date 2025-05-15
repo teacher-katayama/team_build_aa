fe2d07924ae0116f6682c4a934bb207a410f3078
katayama noriyuki <katayama@ocsjoho.onmicrosoft.com>
2025-05-15 14:30:53 +0900

    Html + JavaScriptのサンプルコード

---
+++ sample.js
@@ -0,0 +1,14 @@
+const word_a = "朝から驚きの連続だ";
+const word_i = "いつもの朝とは様子が違う";
+const word_u = "嬉しい知らせが飛び込んできた";
+const word_e = "駅で急に待ち合わせ。慌てて準備して";
+const word_o = "温泉旅行に出発だ！";
+
+const outputDiv = document.getElementById("output");
+outputDiv.innerHTML = `
+    <p>あ：${word_a}</p>
+    <p>い：${word_i}</p>
+    <p>う：${word_u}</p>
+    <p>え：${word_e}</p>
+    <p>お：${word_o}</p>
+`;
