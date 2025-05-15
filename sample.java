10b256a4c769a9a99f422d0a5a2d3ddeacb517cd
katayama noriyuki <katayama@ocsjoho.onmicrosoft.com>
2025-05-15 14:30:27 +0900

    Javaのサンプルコード

---
+++ sample.java
@@ -0,0 +1,15 @@
+public class sample {
+    public static void main(String[] args) {
+        String word_a = "朝から驚きの連続だ";
+        String word_i = "いつもの朝とは様子が違う";
+        String word_u = "嬉しい知らせが飛び込んできた";
+        String word_e = "駅で急に待ち合わせ。慌てて準備して";
+        String word_o = "温泉旅行に出発だ！";
+
+        System.out.println("あ：" + word_a);
+        System.out.println("い：" + word_i);
+        System.out.println("う：" + word_u);
+        System.out.println("え：" + word_e);
+        System.out.println("お：" + word_o);
+    }
+}
