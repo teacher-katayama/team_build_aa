d0970382d732e550194faf5689601b98a85ee4f5
katayama noriyuki <katayama@ocsjoho.onmicrosoft.com>
2025-05-15 14:30:15 +0900

    Pythonのサンプルコード

---
+++ sample.py
@@ -0,0 +1,12 @@
+word_a = "朝から驚きの連続だ"
+word_i = "いつもの朝とは様子が違う"
+word_u = "嬉しい知らせが飛び込んできた"
+word_e = "駅で急に待ち合わせ。慌てて準備して"
+word_o = "温泉旅行に出発だ！"
+
+
+print(f"あ：{word_a}")
+print(f"い：{word_i}")
+print(f"う：{word_u}")
+print(f"え：{word_e}")
+print(f"お：{word_o}")
