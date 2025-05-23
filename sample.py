405577fa0af4e37a15e0f09aaa820a9cd19e9dc9
Yoichi Setoyama <setoyama.yoichi@gmail.com>
2025-05-23 09:18:43 +0900

    更新

--- sample.py
+++ sample.py
@@ -1,12 +1,20 @@
-word_a = "朝から驚きの連続だ"
-word_i = "いつもの朝とは様子が違う"
-word_u = "嬉しい知らせが飛び込んできた"
-word_e = "駅で急に待ち合わせ。慌てて準備して"
-word_o = "温泉旅行に出発だ！"
+from google import genai
+import tkinter
 
+winodw = tkinter.Tk()
+winodw.title("あいうえお作文生成")
 
-print(f"あ：{word_a}")
-print(f"い：{word_i}")
-print(f"う：{word_u}")
-print(f"え：{word_e}")
-print(f"お：{word_o}")
+winodw.mainloop()
+
+client = genai.Client(api_key="API KEY入れてね")
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="あいうえお作文を作成してください。\
+            テーマはあなたが決めてください。\
+            いろいろなテーマにしてください。\
+            例に習って記述してください。\n\nあ：あなたの笑顔が\nい：いつも私を\nう：嬉しくさせる\nえ：笑顔の理由は\nお：お互いの信頼\
+            あいうえお作文のみを記述してください。\
+            ひとつのみ記述してください。",
+)
+print(response.text)
