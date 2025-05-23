02a0e39933191de42a883881f65f50dc0309f0d6
fujii haruhito <kaihatu.ocs@gmail.com>
2025-05-23 09:41:35 +0900

    プルダウン

--- sample.py
+++ sample.py
@@ -1,12 +1,43 @@
-word_a = "朝から驚きの連続だ"
-word_i = "いつもの朝とは様子が違う"
-word_u = "嬉しい知らせが飛び込んできた"
-word_e = "駅で急に待ち合わせ。慌てて準備して"
-word_o = "温泉旅行に出発だ！"
+import tkinter
+from tkinter import ttk
+
+from google import genai
+
+winodw = tkinter.Tk()
+winodw.title("あいうえお作文生成")
 
 
-print(f"あ：{word_a}")
-print(f"い：{word_i}")
-print(f"う：{word_u}")
-print(f"え：{word_e}")
-print(f"お：{word_o}")
+def on_select(event):
+    selected = combo.get()
+    print(f"選択された文字: {selected}")
+
+
+winodw.title("プルダウンメニュー")
+
+# 選択肢リスト
+choices = ["あ", "か", "さ", "た", "な", "は", "ま", "や", "ら", "わ"]
+
+combo = ttk.Combobox(
+    winodw, values=choices, state="readonly"
+)  # readonlyにすると直接入力できない
+combo.current(0)  # 初期選択は「あ」
+combo.pack(padx=20, pady=20)
+
+combo.bind("<<ComboboxSelected>>", on_select)
+
+winodw.mainloop()
+
+winodw.mainloop()
+
+client = genai.Client(api_key="AIzaSyAI1-EwRlxhJXHaaskbxY1Xyjus1QP9wFM")
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="あいうえお作文を作成してください。\
+            テーマはあなたが決めてください。\
+            いろいろなテーマにしてください。\
+            例に習って記述してください。\n\nあ：あなたの笑顔が\nい：いつも私を\nう：嬉しくさせる\nえ：笑顔の理由は\nお：お互いの信頼\
+            あいうえお作文のみを記述してください。\
+            ひとつのみ記述してください。",
+)
+print(response.text)
